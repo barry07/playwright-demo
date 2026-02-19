76a5ef6339f2c506f2bd7ffca0528cd7eefd8e75
Barry Horgan <horgan.barry@gmail.com>
2026-02-19 18:39:26 +1100

    updated test spec

--- tests/demo.spec.js
+++ tests/demo.spec.js
@@ -1,10 +1,17 @@
 const { test, expect } = require('@playwright/test');
 
-test('Check Docker App Status', async ({ page }) => {
-  // 1. Tell the browser to visit your running Docker container
+test('Reveal secret message', async ({ page }) => {
+  // 1. goto app
   await page.goto('http://localhost:8080');
 
-  // 2. Expect the heading to contain our specific text
-  const heading = page.locator('h1');
-  await expect(heading).toContainText('CI/CD Pipeline active.');
+  // 2. Verify message is HIDDEN at first
+  const hmessage = page.locator('#message');
+  await expect(message).toBeHidden();
+
+  // 3. Click the button to reveal the message
+  await page.click('#secret-button');
+
+  // 4. Verify message is VISIBLE after clicking the button
+  await expect(hmessage).toBeVisible();
+  await expect(message).toHaveText('Playwright found me!');
 });
